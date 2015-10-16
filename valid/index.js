505e791c192fab2761496d90fb8ef40b64cf1754
hyp <954957486@qq.com>
2015-10-16 18:14:29 +0800

    add ajax

--- valid/index.js
+++ valid/index.js
@@ -1,14 +1,54 @@
 $(function(){
+	/*$.validator.setDefaults({
+		submitHandler: function() {
+			$.ajax({
+				type:'post',
+				url:'back.json',
+				datyType:'json',
+				data:'1',
+				success:function(data){
+					alert(data.name);
+				},
+				error:function(XMLHttpRequest, textStatus, errorThrown){
+                     console.log(XMLHttpRequest.status);//200
+                        console.log(XMLHttpRequest.readyState);//4
+                        console.log(textStatus); //parsererror   
+                    console.log('fail');
+                }
+			});
+		}
+	});*/
+	$('#form1').validate({
+		
+	});
 	$('#form').validate({
+		/*debug:false,*/
 		messages:{
 			user:'请输入中文',
-			password:'',
+			password:'密码格式错误',
 			phone:'手机号码格式错误'
 		},
 		rules:{
 			user:'required',
 			password:'required',
 			phone:'required'
+		},
+		submitHandler:function(form) {
+			$.ajax({
+				type:'post',
+				url:'back.json',
+				datyType:'json',
+				data:'1',
+				success:function(data){
+					alert(data.name);
+				},
+				error:function(XMLHttpRequest, textStatus, errorThrown){
+                     console.log(XMLHttpRequest.status);//200
+                        console.log(XMLHttpRequest.readyState);//4
+                        console.log(textStatus); //parsererror   
+                    console.log('fail');
+                }
+			});
 		}
 	});
 });
