b48407d9eccd3b62fde5429404013bd01947ed2d
hyp <954957486@qq.com>
2015-11-13 10:29:44 +0800

    s

--- valid/index.js
+++ valid/index.js
@@ -1,4 +1,7 @@
-$(function(){
+define(['jquery','validate'],function($,validate){
+	$(function(){
+	$.extend({validate:function(){}});
+	$.validate({});
 	/*$.validator.setDefaults({
 		submitHandler: function() {
 			$.ajax({
@@ -51,4 +54,66 @@ $(function(){
 			});
 		}
 	});
+	$("#form2").validate({
+			messages:{
+				user:'请输入中文',
+				password:{
+					required:'密码格式错误',
+					min:'太小'
+				},
+				phone:'手机号码格式错误'
+			},
+			rules: {
+				user:{
+					required:{
+						depends: function(element) {
+					      return $("#form2 input[name=password]").val()!=''?true:false;
+					    }
+					}
+				},
+				password: {
+				  required: true,
+				  min: {
+				    // min needs a parameter passed to it
+				    param: 6,
+				    depends: function(element) {
+				      return $("#form2 input[name=user]").val()!=''?true:false;
+				    }
+				  }
+				}
+			}
+	});
+	$("#form3").validate({
+			messages:{
+				user:'请输入中文',
+				password:{
+					required:'密码格式错误',
+					min:'太小'
+				},
+				phone:'手机号码格式错误'
+			},
+			rules: {
+				user:{
+					required:true
+					
+				},
+				password: {
+				  required: true,
+				  min: {
+				    // min needs a parameter passed to it
+				    param: 6,
+				    depends: function(element) {
+				      return $("#form3 input[name=user]").val()!=''?true:false;
+				    }
+				  }
+				}
+			},
+			invalidHandler: function(event, validator) {
+			    // 'this' refers to the form
+			    var errors = validator.numberOfInvalids();
+			    alert(errors);
+			    return;
+		    }
+	});
+});
 });
---
+++ valid/main.js
@@ -0,0 +1,8 @@
+require.config({
+	paths:{
+		'jquery' : 'jquery',
+		'validate' : 'validate',
+	}
+});
+require(['index'],function() {
+});
