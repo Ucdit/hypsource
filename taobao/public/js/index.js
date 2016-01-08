f420546df3b8174dd378ef1359965be311b16d43
Ucdit <954957486@qq.com>
2016-01-08 14:57:39 +0800

    s\

--- taobao/public/js/index.js
+++ taobao/public/js/index.js
@@ -8,32 +8,100 @@ var ele=document.getElementById("myhead");
 ele.addEventListener('touchstart',function(e){
     alert(e.touches.length);
 });
-slider({});
-function slider(opt){
+var slide=new Slider({direction:'left'});
+    slide.init();
+function Slider(opt){
     this.option={};
-    this.option.element=document.getElementById(this.option.element||'#slider');
+    this.option.element=document.getElementById(this.option.element||'slider');
     this.option.direction=opt.direction||'left';
     this.option.loop=opt.loop||'false';//默认不循环;
     this.option.autoPlay=opt.autoPlay||'true';
     this.option.spaceTime=opt.spaceTime||3000;//毫秒为单位,间隔时间;
     this.option.speed=opt.speed||1;
-    this.init();
+    /*this.init();*/
+    this.init=function(){
+        var element=this.option.element,
+            direction=this.option.direction,
+            scrollWidth,
+            autoPlay=this.option.autoPlay;
+        /*获得移动宽度*/
+        this.scrollWidth=document.body.clientWidth;
+        /*获得li的个数，banner个数*/
+        console.log(this.option.element);
+        this.size=this.option.element.children[1].children[0].children.length;
+        this.index=0;
+        this.slideBlock=this.option.element.children[1];
+        this.slideBlock.style.width=this.scrollWidth*this.size+'px';
+        this.doTouch(this.slideBlock);
+    };
+    this.doTouch=function(ele){
+        var _this=this;
+        ele.addEventListener('touchstart',function(e){
+            console.log('start');
+            var point= e.touches[0];
+            _this.startX=point.pageX;
+            _this.startY=point.pageY;
+        });
+        ele.addEventListener('touchmove',function(e){
+            console.log('move');
+            if(e.touches.length>1|| e.scale&& e.scale!==1){
+                return;
+            }
+            else{
+                var point= e.touches[0];
+                _this.distX=point.pageX-_this.startX;
+                _this.distY=point.pageY-_this.startY;
+                /*手指移动了距离*/
+                if(Math.abs(_this.distX)>0){
+                    /*获得高度，先计算即将显示的内容的index*/
+                    if(_this.index==0&&_this.distX>0||_this.index==_this.size-1&&_this.distX<0)
+                    {
+                        _this.move(-_this.scrollWidth*_this.index+_this.distX*0.4,_this.option.speed,this);
+                    }
+                    else{
+                        _this.move(-_this.scrollWidth*_this.index+_this.distX,_this.option.speed,this);
+                    }
+                }
+            }
+        });
+        ele.addEventListener('touchend',function(e){
+            console.log('end');
+            console.log(_this.distX);
+            if(Math.abs(_this.distX)>0){
+                if(Math.abs(_this.distX)>_this.scrollWidth/10&&_this.distX>0){
+                    console.log('>0');
+                    _this.index=_this.index==0?_this.index:_this.index-1;
+                }
+                if(Math.abs(_this.distX)>_this.scrollWidth/10&&_this.distX<0){
+                    console.log('<0');
+                    _this.index=_this.index==_this.size-1?_this.index:_this.index+1;
+                    console.log(_this.index);
+                }
+                _this.move(-_this.scrollWidth*_this.index,_this.option.speed,this);
+            }
+        });
+    };
+    this.move=function (width,speed,ele){
+        ele.style.transition='transform '+speed+'s';
+        ele.style.transform='translate('+width+'px,0)';
+
+    };
 }
-slider.prototype.init=function(){
+/*Slider.prototype.init = function(){
     var element=this.option.element,
         direction=this.option.direction,
         scrollWidth,
         autoPlay=option.autoPlay;
-    /*获得移动宽度*/
+    /!*获得移动宽度*!/
     this.scrollWidth=document.body.clientWidth;
-    /*获得li的个数，banner个数*/
+    /!*获得li的个数，banner个数*!/
     this.size=this.option.element.children[1].children[0].children.length;
     this.index=0;
     this.slideBlock=this.option.element.children[1];
-    this.slideBlock.style.width=this.scrollWidth*this.size+'px'
+    this.slideBlock.style.width=this.scrollWidth*this.size+'px';
     this.doTouch(this.slideBlock);
-}
-slider.prototype.doTouch=function(ele){
+};
+Slider.prototype.doTouch=function(ele){
     var _this=this;
     ele.addEventListener('touchstart',function(e){
         console.log('start');
@@ -50,9 +118,9 @@ slider.prototype.doTouch=function(ele){
             var point= e.touches[0];
             _this.distX=point.pageX-_this.startX;
             _this.distY=point.pageY-_this.startY;
-            /*手指移动了距离*/
+            /!*手指移动了距离*!/
             if(Math.abs(_this.distX)>0){
-                /*获得高度，先计算即将显示的内容的index*/
+                /!*获得高度，先计算即将显示的内容的index*!/
                 if(_this.index==0&&_this.distX>0||_this.index==_this.size-1&&_this.distX<0)
                 {
                     _this.move(_this.scrollWidth*_this.index+_this.distX*0.4,_this.option.speed,this);
@@ -76,11 +144,8 @@ slider.prototype.doTouch=function(ele){
         }
     });
 }
-slider.prototype.play=function(){
-
-}
-slider.prototype.move=function (width,speed,ele){
+Slider.prototype.move=function (width,speed,ele){
     ele.style.transition='transform '+speed+'s';
     ele.style.transform='translate('+width+'px,0)';
 
-}
+}*/
