4f2dd3c186398cc35f9b6da0a26afbf9dd0c80b9
hyp <954957486@qq.com>
2016-01-15 20:21:33 +0800

    d

--- taobao/public/js/index.js
+++ taobao/public/js/index.js
@@ -3,21 +3,23 @@
 * next页面
 * scrollWidth滚动多长
 * 先默认宽度为屏幕宽度
+* 2016.1.5添加新功能----自动播放
 */
 var ele=document.getElementById("myhead");
 ele.addEventListener('touchstart',function(e){
     alert(e.touches.length);
 });
-var slide=new Slider({direction:'left',loop:true});
+var slide=new Slider({direction:'left',loop:false});
     slide.init();
 function Slider(opt){
     this.option={};
     this.option.element=document.getElementById(this.option.element||'slider');
     this.option.direction=opt.direction||'left';
     this.option.loop=opt.loop||false;//默认不循环;
-    this.option.autoPlay=opt.autoPlay||'true';
+    this.option.autoPlay=opt.autoPlay||true;
     this.option.spaceTime=opt.spaceTime||3000;//毫秒为单位,间隔时间;
     this.option.speed=opt.speed||200;
+    this.autoClear=null;
     /*this.init();*/
     this.init=function(){
         var element=this.option.element,
@@ -33,9 +35,15 @@ function Slider(opt){
         this.addEle(this.option.loop);
         this.slideBlock=this.option.element.children[1];
         this.slideBlock.style.width=this.scrollWidth*this.slideBlock.children[0].children.length+'px';
-        this.move(-this.scrollWidth,0,this.slideBlock);
+        if(this.option.loop){
+            this.move(-this.scrollWidth,0,this.slideBlock);
+        }
         this.doTouch(this.slideBlock);
+        if(this.option.autoPlay){
+            this.autoGo();
+        }
     };
+    /*循环添加前后li项*/
     this.addEle=function(isLoop){
         var ul=this.option.element.children[1].children[0];
         var li=ul.children;
@@ -44,6 +52,7 @@ function Slider(opt){
             ul.appendChild(li[1].cloneNode(true));
         }
     };
+    /*事件处理*/
     this.doTouch=function(ele){
         var _this=this;
         ele.addEventListener('touchstart',function(e){
@@ -53,6 +62,7 @@ function Slider(opt){
             _this.startY=point.pageY;
         });
         ele.addEventListener('touchmove',function(e){
+            clearInterval(_this.autoClear);
             e.preventDefault();
             if(e.touches.length>1|| e.scale&& e.scale!==1){
                 return;
@@ -77,52 +87,65 @@ function Slider(opt){
         ele.addEventListener('touchend',function(e){
             var me=this;
             e.preventDefault();
-            if(Math.abs(_this.distX)>0){
-                if(Math.abs(_this.distX)>_this.scrollWidth/10&&_this.distX>0){
-                    if(_this.option.loop){
-                        _this.index=_this.index==0?_this.size-1:_this.index-1;
-                        //_this.index=_this.index==0?_this.index:_this.index-1;
-                        //_this.index==_this.size-1?setTimeout(function(){_this.move(-_this.scrollWidth*_this.size,0,me)},_this.option.speed):_this.move(-_this.scrollWidth*(_this.index+1),_this.option.speed,this);
-                        if(_this.index==_this.size-1){
-                            _this.move(0,_this.option.speed,me);
-                            setTimeout(function(){_this.move(-_this.scrollWidth*_this.size,0,me)},_this.option.speed);
-                        }else{
-                            _this.move(-_this.scrollWidth*(_this.index+1),_this.option.speed,this)
-                        }
-                        /*doted*/
-                        _this.dotted(_this.dotLi,_this.index);
-                    }
-                    else{
-                        _this.index=_this.index==0?_this.index:_this.index-1;
-                        _this.move(-_this.scrollWidth*_this.index,_this.option.speed,this);
-                        /*doted*/
-                        _this.dotted(_this.dotLi,_this.index);
-                    }
-                }
-                if(Math.abs(_this.distX)>_this.scrollWidth/10&&_this.distX<0){
-                    if(_this.option.loop){
-                        _this.index=_this.index==_this.size-1?0:_this.index+1;
-                        //_this.move(-_this.scrollWidth*(_this.index+1),_this.option.speed,this)
-                        //_this.index==0?setTimeout(function(){_this.move(-_this.scrollWidth,0,me)},_this.option.speed):'';
-                        if(_this.index==0){
-                            _this.move(-_this.scrollWidth*(_this.size+1),_this.option.speed,me);
-                            setTimeout(function(){_this.move(-_this.scrollWidth,0,me)},_this.option.speed);
-                        }else{
-                            _this.move(-_this.scrollWidth*(_this.index+1),_this.option.speed,this)
-                        }
-                        /*doted*/
-                        _this.dotted(_this.dotLi,_this.index);
-                    }
-                    else{
-                        _this.index=_this.index==_this.size-1?_this.index:_this.index+1;
-                        _this.move(-_this.scrollWidth*_this.index,_this.option.speed,this);
-                        /*doted*/
-                        _this.dotted(_this.dotLi,_this.index);
-                    }
-                }
-
-            }
+            _this.play(me);
+            _this.autoGo();
         });
+    };
+    this.play=function(me){
+        console.log(this.index);
+        var _this=this;
+        if(Math.abs(_this.distX)>0){
+            if(Math.abs(_this.distX)>_this.scrollWidth/10&&_this.distX>0){
+                if(_this.option.loop){
+                    _this.index=_this.index==0?_this.size-1:_this.index-1;
+                    //_this.index=_this.index==0?_this.index:_this.index-1;
+                    //_this.index==_this.size-1?setTimeout(function(){_this.move(-_this.scrollWidth*_this.size,0,me)},_this.option.speed):_this.move(-_this.scrollWidth*(_this.index+1),_this.option.speed,this);
+                    if(_this.index==_this.size-1){
+                        _this.move(0,_this.option.speed,me);
+                        setTimeout(function(){_this.move(-_this.scrollWidth*_this.size,0,me)},_this.option.speed);
+                    }else{
+                        _this.move(-_this.scrollWidth*(_this.index+1),_this.option.speed,me)
+                    }
+                    /*doted*/
+                    _this.dotted(_this.dotLi,_this.index);
+                }
+                else{
+                    _this.index=_this.index==0?_this.index:_this.index-1;
+                    _this.move(-_this.scrollWidth*_this.index,_this.option.speed,me);
+                    /*doted*/
+                    _this.dotted(_this.dotLi,_this.index);
+                }
+            }
+            if(Math.abs(_this.distX)>_this.scrollWidth/10&&_this.distX<0){
+                if(_this.option.loop){
+                    _this.index=_this.index==_this.size-1?0:_this.index+1;
+                    //_this.move(-_this.scrollWidth*(_this.index+1),_this.option.speed,this)
+                    //_this.index==0?setTimeout(function(){_this.move(-_this.scrollWidth,0,me)},_this.option.speed):'';
+                    if(_this.index==0){
+                        _this.move(-_this.scrollWidth*(_this.size+1),_this.option.speed,me);
+                        setTimeout(function(){_this.move(-_this.scrollWidth,0,me)},_this.option.speed);
+                    }else{
+                        _this.move(-_this.scrollWidth*(_this.index+1),_this.option.speed,me)
+                    }
+                    /*doted*/
+                    _this.dotted(_this.dotLi,_this.index);
+                }
+                else{
+                    _this.index=_this.index==_this.size-1?_this.index:_this.index+1;
+                    _this.move(-_this.scrollWidth*_this.index,_this.option.speed,me);
+                    /*doted*/
+                    _this.dotted(_this.dotLi,_this.index);
+                }
+            }
+        }
+    };
+    this.autoGo=function(){
+        var _this=this;
+        this.autoClear=setInterval(function(){
+            /*判断是不是循环播放，，根据index*/
+            _this.distX=-_this.scrollWidth/4;
+            _this.play(_this.slideBlock);
+        },this.option.spaceTime);
     };
     this.move=function (width,speed,ele){
         ele.style.transition='transform '+speed+'ms';
@@ -132,7 +155,6 @@ function Slider(opt){
     this.dotted=function(lis,index){
         var name_id;
         /*把当前active的元素的active去掉*/
-
         for(var i=0;i<lis.length;i++){
             name_id=this.hasClass(lis[i],'active');
             if(name_id===false){
