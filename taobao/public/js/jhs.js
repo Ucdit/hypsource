d90060fc729dc80f49e9d63ba014d38a11e0f1df
hyp <954957486@qq.com>
2016-01-29 17:56:27 +0800

    d

--- taobao/public/js/jhs.js
+++ taobao/public/js/jhs.js
@@ -12,4 +12,27 @@ scroll.addEventListener('webkitTransitionEnd',function(){
     scroll.style.transform='translate('+(200)+'px,0)';
     scroll.style.transition='transform 0.2s cubic-bezier(0.25, 0.1, 0.25, 1) 0s';
 })*/
+(function(){
+    function Face(opt){
+        this.option={
+            wall:opt.wall,
+            holes:opt.holes,
+            imgs:opt.imgs,
+            widthCell:opt.widthCell,
+            rows:opt.rows,
+            column:opt.column
+        };
+    };
+    Face.prototype.init=function(){
 
+    };
+    Face.prototype.creatHole=function(){
+        var _this=this;
+        var faceWall=_this.option.rows*_this.option.column;
+        for(var i=0;i<faceWall;i++){
+            _this.option.wall.appendChild()
+        }
+    };
+    var faceBook=new Face();
+    faceBook.init();
+})();
